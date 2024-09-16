9139beb030caec0a8436b25f76569a6a85680937
mattiaponci <poncini.mattia@gmail.com>
2024-09-16 23:48:14 +0200

    add extensions

--- flotipios/Controller/LoginVC.swift
+++ flotipios/Controller/LoginVC.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class LoginVCViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+       
+        
     }
     
 
