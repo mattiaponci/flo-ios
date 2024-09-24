af1d8c55545462cb6e5b4def6a08cc650d60a334
mattiaponci <poncini.mattia@gmail.com>
2024-09-24 21:12:34 +0200

    reset password view

---
+++ flotipios/Controller/ForgotPasswordVC.swift
@@ -0,0 +1,29 @@
+//
+//  ForgotPasswordVC.swift
+//  flotipios
+//
+//  Created by mattia poncini on 24.09.2024.
+//
+
+import UIKit
+
+class ForgotPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
